5da62e40aa170845a78df848a1184748a0d20260
王传辉 <372020909@qq.com>
2017-07-31 09:44:25 +0800

    添加依赖

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 let package = Package(
     name: "MySQLORM",
     dependencies: [
-        .Package(
-            url:"https://github.com/PerfectlySoft/Perfect-MySQL.git",
-            majorVersion: 2
-        )
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2)
     ]
 )
